ea770f302049d228d0d0759679012608fedd67d2
bobomurod <bobomurod.boboev@yandex.ru>
2025-03-13 23:47:12 +0300

    fresh start

--- internal/core/domain/product.go
+++
@@ -1,6 +0,0 @@
-package domain
-
-type product struct {
-	Id   int    `json:"id"`
-	Name string `json:"name"`
-}
--- internal/core/ports/mongodb.go
+++
@@ -1,9 +0,0 @@
-package ports
-
-import "context"
-
-type DatabaseClient interface {
-	Connect(ctx context.Context) error
-	Close(ctx context.Context) error
-	Ping(ctx context.Context) error
-}
